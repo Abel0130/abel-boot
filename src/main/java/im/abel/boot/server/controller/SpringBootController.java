1c29c8bec646b6fe6f320baf0a6ff7eddcea4600
abel1225 <abel0130@hotmail.com>
2017-05-21 11:22:10 +0800

    build

---
+++ src/main/java/im/abel/boot/server/controller/SpringBootController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package im.abel.boot.server.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description 
+ * @author 李真(Abel.li)
+ * @contact abel0130@163.com
+ * @version 
+ */
+@RestController
+@EnableAutoConfiguration
+public class SpringBootController {
+
+}
